e7fcfd9dadbf03bebd6bbc3fdf3b867fcf34995d
arsen <arsen@ARSENIPC>
2026-01-29 01:57:04 +0200

    Add project files.

---
+++ Kordamine/Kordamine.py
@@ -0,0 +1,17 @@
+import random * -
+# n = int(input("Sisesta kuuskede arv (1–9): "))
+
+# for i in range(n):
+#     print("   /V\\    ")
+#     print("  / V \\   ")
+#     print(" / V V \\  ")
+#     print("/VV V VV\\ ")
+
+# R = int(input("Sisesta arv R: "))
+# korrutamine=1
+# for i in range(1, R + 1):
+#     if i % 2 == 1:
+#         korrutamine = korrutamine * i
+#         print("Paaritu arvu korrutis: ", korrutamine)
+n=кфr   randint(1,100)
+print(n)
