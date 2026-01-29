8d1425a2d43e25e2502f6d27b4a99d94cf50f390
arsen <arsen@ARSENIPC>
2026-01-29 02:16:48 +0200

    dsa

--- Kordamine/Kordamine.py
+++ Kordamine/Kordamine.py
@@ -1,17 +1,44 @@
-import random * -
+import random
 # n = int(input("Sisesta kuuskede arv (1–9): "))
-
+#1
 # for i in range(n):
 #     print("   /V\\    ")
 #     print("  / V \\   ")
 #     print(" / V V \\  ")
 #     print("/VV V VV\\ ")
-
+#2
 # R = int(input("Sisesta arv R: "))
 # korrutamine=1
 # for i in range(1, R + 1):
 #     if i % 2 == 1:
 #         korrutamine = korrutamine * i
 #         print("Paaritu arvu korrutis: ", korrutamine)
-n=кфr   randint(1,100)
-print(n)
+#3
+# pos = 0
+# n=random.randint(-100,100)
+# print(n)
+# if n>0:
+#     pos += 1
+#     print("positiivne arv: ", pos)
+# else:
+#     print("mittepositiivne arv")
+#4
+# arv = input("Sisesta naturaalarv: ")
+# Paaris = 0
+# Paaritu = 0
+# for i in arv:
+#     if int(i) % 2 == 0:
+#         Paaris += 1
+#     else:
+#         Paaritu += 1
+
+# print("Paaris numbreid on: ", Paaris)
+# print("Paaritu numbreid on: ", Paaritu)
+#5
+# A = int(input("Sisesta A: "))
+# B = int(input("Sisesta B: "))
+# S = 0
+# for i in range(A,B+1):
+#     S += i
+
+# print("Arvude summa", A, "kuni", B, "võrdub", S)
